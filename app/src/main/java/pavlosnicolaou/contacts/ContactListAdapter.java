6b757ecf0fafb85cef71ea5243bf9e769100fd09
p4vlos <nicolaou_paul@hotmail.com>
2017-02-17 10:27:30 +0000

    creating the Recycler view and an adaptor to show raw data in it

---
+++ app/src/main/java/pavlosnicolaou/contacts/ContactListAdapter.java
@@ -0,0 +1,16 @@
+package pavlosnicolaou.contacts;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by vl1s on 17/02/2017.
+ */
+
+public class ContactListAdapter {
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+}
--- app/src/main/java/pavlosnicolaou/contacts/MainActivity.java
+++ app/src/main/java/pavlosnicolaou/contacts/MainActivity.java
@@ -2,12 +2,30 @@ package pavlosnicolaou.contacts;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView contactListView;
+    private LinearLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Wire recycler view
+        contactListView = (RecyclerView) findViewById(R.id.contact_list_view);
+
+        //Setup layout manager
+        layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        layoutManager.scrollToPosition(0);
+
+        //Attach to recycler view
+        contactListView.setLayoutManager(layoutManager);
+
     }
 }
